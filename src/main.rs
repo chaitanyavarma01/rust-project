35300af46720e88e6c08cccee1e96328f015e559
chaitanyavarma01 <abchaitanya0@gmail.com>
2025-03-19 14:31:25 +0000

    v2

--- src/main.rs
+++ src/main.rs
@@ -3,14 +3,45 @@ use rand::Rng;
 use std::fs::File;
 use std::io::prelude::*;
 
-struct Main {
+use std::io;
+
+const ALLOWED_ATTEMPTS: u8 = 5;
+struct Letter {
+    character:char,
+    revealed:bool,
 }
 
 
 fn main(){
     let selected_word = selected_word();
+    let mut turns_left = ALLOWED_ATTEMPTS;
+    let mut letters = create_letter(&selected_word);
 
-    println!("The selected word was {}", selected_word)
+
+    loop {
+        println!("You have {} turns left", turns_left);
+        display_function(&letters);
+
+        println!("Please enter a letter to guess:");
+        let user_char = read_user_input();
+
+        if user_char == '*'{
+            break;
+        }
+
+        let mut at_least_one_revealed = false;
+        for letter in letters.iter_mut(){
+            if letter.character == user_char {
+                letter.revealed = true;
+                at_least_one_revealed = true;
+            }
+
+        }
+        
+        if !at_least_one_revealed{
+            turns_left -= 1;
+        }
+    }
 }
 
 fn selected_word() -> String {
@@ -25,4 +56,46 @@ fn selected_word() -> String {
 
     return String::from(available_words[random_index]);
  
+}
+
+fn create_letter(word: &String) -> Vec<Letter> {
+    let mut letters: Vec<Letter> = Vec::new();
+
+    for c in word.chars(){
+        letters.push(Letter{
+            character : c,
+            revealed : false,
+        })
+    }
+    return letters;
+
+}
+
+fn display_function(letters:&Vec<Letter>) {
+    let mut display_string = String::from("Progress:");
+
+    for letter in letters {
+        display_string.push(' ');
+
+        if letter.revealed {
+            display_string.push(letter.character);
+        } else {
+            display_string.push('_');
+        }
+        display_string.push(' ');
+    }
+    println!("{}", display_string);
+}
+
+fn read_user_input() -> char {
+    let mut user_input =  String::new();
+    match io::stdin().read_line(&mut user_input) {
+        Ok(_) => {
+            match user_input.chars().next() {
+                Some(c) => { return c; }
+                None => { return '*'; }
+            }
+        }
+        Err(_) => { return '*'; }
+    }
 }
